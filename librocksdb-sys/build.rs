356fd5e3f6b891c7d707dc91aee86e004cc4afb2
Jason Volk <jason@zemos.net>
2025-11-22 08:49:24 +0000

    static liburing
    Signed-off-by: Jason Volk <jason@zemos.net>

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -307,6 +307,13 @@ fn build_rocksdb() {
         pkg_config::probe_library("liburing")
             .expect("The io-uring feature was requested but the library is not available");
         config.define("ROCKSDB_IOURING_PRESENT", Some("1"));
+
+        let mode = if cfg!(feature = "static") {
+            "=static"
+        } else {
+            ""
+        };
+        println!("cargo:rustc-link-lib{mode}=uring");
     }
 
     if &target != "armv7-linux-androideabi"
